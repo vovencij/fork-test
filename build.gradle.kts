4010f7fb49e79ac21e329f9048b5858d1e73e061
Vladimir Sor <vsor@splunk.com>
2020-11-19 11:44:53 +0200

    Windows containers.

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  java
+  groovy
+}
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+
+  // Use the latest Groovy version for Spock testing
+  testImplementation("org.codehaus.groovy:groovy-all:2.5.11")
+  testImplementation("org.testcontainers:spock:1.15.0")
+
+  // Use the awesome Spock testing and specification framework even with Java
+  testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
+  testImplementation("junit:junit:4.13")
+}
+
