8ec285204498ffe74f82b9f306b7cc792346d504
Vladimir Sor <vsor@splunk.com>
2020-11-19 18:37:43 +0200

    Docker build + compose + test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
 
   // Use the latest Groovy version for Spock testing
   testImplementation("org.codehaus.groovy:groovy:2.5.11")
+  testImplementation("org.slf4j:slf4j-api:1.7.30")
 
   // Use the awesome Spock testing and specification framework even with Java
   testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
